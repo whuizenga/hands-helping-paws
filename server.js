04dfd90e3c693f36d347469c6da9135b38088142
whuizenga <will.huizenga@gmail.com>
2017-11-14 13:27:37 -0500

    created server boilerplate

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');    
+}); 
+
+// If the connection throws an error
+connection.on('error', (err) => {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/client/src/'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/src/index.html');
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Server is up on port: " + PORT);
+});
