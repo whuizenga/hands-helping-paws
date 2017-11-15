0057f2297449d4444a1896424ce02ac9443c3c94
whuizenga <will.huizenga@gmail.com>
2017-11-15 16:07:46 -0500

    Now handing back the pets successfully!

--- controllers/adoptionsController.js
+++ controllers/adoptionsController.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     axios.get(`https://api.petfinder.com/shelter.getPets?key=${process.env.PETFINDERKEY}&id=OK252&format=json`).then((response) => {
-        res.json(response.data);
+        res.json(response.data.petfinder.pets);
     }).catch((err) => {
         console.log(err)
     })
